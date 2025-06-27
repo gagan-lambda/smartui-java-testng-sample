23d33a259ed9e078259db3132a6b7626c7c11ffc
gagan-lambda <gagandeepb@lambdatest.com>
2025-06-27 18:58:37 +0530

    SDK with status update

--- src/test/java/com/lambdatest/sdk/SmartUISDKCloud.java
+++ src/test/java/com/lambdatest/sdk/SmartUISDKCloud.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import java.util.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -14,17 +15,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import io.github.lambdatest.SmartUISnapshot;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.json.JSONObject;
 
 public class SmartUISDKCloud {
 
     private RemoteWebDriver driver;
     private String Status = "failed";
+    private String projectToken = "2838#cad0c854-kdjd-4c08-80d7-3146d83e0cf0#veeva-sdk"; // Update your own project token
     private String githubURL = System.getenv("GITHUB_URL");
+    private String buildId = System.getenv("SMARTUI_BUILD_ID");
+    private String buildName = System.getProperty("SMARTUI_BUILD_NAME");
+    private String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+    private String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
 
     @BeforeMethod
     public void setup(Method m, ITestContext ctx) throws MalformedURLException {
-        String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
-        String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+      //  String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        //String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
         String hub = "@hub.lambdatest.com/wd/hub";
 
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -32,6 +41,7 @@ public class SmartUISDKCloud {
         caps.setCapability("browserName", "chrome");
         caps.setCapability("version", "latest");
         caps.setCapability("build", "TestNG With Java");
+       // caps.setCapability("smartUI.baseline", true);
         caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
         
         if (githubURL != null) {
@@ -47,20 +57,43 @@ public class SmartUISDKCloud {
     @Test
     public void basicTest() throws Exception {
         System.out.println("Loading Url");
-        driver.get("https://www.lambdatest.com/visual-regression-testing");
+        driver.get("https://flipkart.com");
         Thread.sleep(1000);
-        SmartUISnapshot.smartuiSnapshot(driver, "visual-regression-testing");
-        Thread.sleep(5000);
-        driver.get("https://www.lambdatest.com");
-        Thread.sleep(1000);
-        SmartUISnapshot.smartuiSnapshot(driver, "homepage");
-        Thread.sleep(1000);
-        System.out.println("Test Finished");
+        SmartUISnapshot.smartuiSnapshot(driver, "Flipkart");
+        Thread.sleep(30000);
+        System.out.println("Build ID is : " + buildId);
+
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.executeScript("lambda-status=" + Status);
+String apiUrl = "https://api.lambdatest.com/automation/smart-ui/screenshot/build/status";
+
+        String encodedAuth = Base64.getEncoder().encodeToString((username + ":" + authkey).getBytes());
+
+        Response response = RestAssured
+                .given()
+                    .header("Authorization", "Basic " + encodedAuth)
+                    .header("Accept", "application/json")
+                    .queryParam("projectToken", projectToken)
+                    .queryParam("buildId", buildId)
+
+                .when()
+                    .get(apiUrl)
+                .then()
+                    .statusCode(200)
+                    .extract()
+                    .response();
+
+        JSONObject json = new JSONObject(response.asString());
+        String buildStatus = json.getJSONObject("data").getString("buildStatus");
+
+        System.out.println("Build status is: " + buildStatus);
+
+         Status = buildStatus.equalsIgnoreCase("Rejected") ? "failed" : "passed";
+
+        // Send status back to LambdaTest
+        ((JavascriptExecutor) driver).executeScript("lambda-status=" + Status);
         driver.quit();
     }
 
